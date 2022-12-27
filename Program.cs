cfbc095c6464473a9907980abee44cf7acc76b92
keis94 <keis.vivi@gmail.com>
2022-12-27 18:44:43 +0900

    Impl MFT dump (?)

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.IO;
 using MFT;
 using MFT.Attributes;
 using MFT.Other;
-
+using RawDiskLib;
 using Serilog;
+using Boot;
+using System.ComponentModel;
 
 namespace MFTViewer;
 
@@ -17,17 +20,113 @@ public class MFTViewer
 
         if (args.Length != 1)
         {
-            Log.Error("Usage ./MFTViewer /path/to/$MFT");
+            Log.Error("Usage ./MFTViewer driveLetter");
             Environment.Exit(-1);
         }
 
+        long clusterToByteOffset(long clusterOffset, Boot.Boot boot)
+        {
+            return clusterOffset * boot.SectorsPerCluster * boot.BytesPerSector;
+        }
 
-        string file = args[0];
-        _mft = MftFile.Load(file, false);
+        char driveLetter = args[0][0];
+        RawDisk disk = new RawDisk(driveLetter);
+        using (Stream stream = disk.CreateDiskStream())
+        {
+            Boot.Boot boot = new Boot.Boot(stream);
+            Log.Information("boot.MftClusterBlockNumber: {0}, boot.SectorsPerCluster: {1}, boot.BytesPerSector: {2}, boot.MftEntrySize: {3}", boot.MftClusterBlockNumber, boot.SectorsPerCluster, boot.BytesPerSector, boot.MftEntrySize, boot.RootDirectoryEntries);
 
-        Log.Information("FILE records found: {0} (Free records: {1}) File size: {2}", _mft.FileRecords.Count, _mft.FreeFileRecords.Count, _mft.FileSize);
+            var MFTOffset = clusterToByteOffset(boot.MftClusterBlockNumber, boot);
+            stream.Seek(MFTOffset, SeekOrigin.Begin);
 
-        ProcessRecords(_mft.FileRecords, false, false, "C", ".\\dump", _mft);
+            stream.Seek(0x1c, SeekOrigin.Current);
+            Console.WriteLine("{0:X}", stream.Position);
+
+            byte[] blockSizeBytes = new byte[4];
+            stream.Read(blockSizeBytes, 0, 4);
+            Console.WriteLine("{0:X}", stream.Position);
+
+            stream.Seek(MFTOffset, SeekOrigin.Begin);
+            Console.WriteLine("{0:X}", stream.Position);
+
+            var blockSize = BitConverter.ToInt32(blockSizeBytes, 0);
+            Log.Information("blocksize: {0}", blockSize);
+
+            var fileBytes = new byte[blockSize];
+            stream.Read(fileBytes, 0, blockSize);
+            Console.WriteLine("{0:X}", stream.Position);
+            Log.Information("filebytes: {0}", fileBytes);
+
+            var record = new FileRecord(fileBytes, 0, false);
+
+            Log.Information("ActualRecordSize: {0}", record.ActualRecordSize);
+            foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(record))
+            {
+                string name = descriptor.Name;
+                object value = descriptor.GetValue(record);
+                Console.WriteLine("{0}={1}", name, value);
+            }
+
+            foreach (var attr in record.Attributes)
+            {
+                Console.WriteLine("==================");
+                foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(attr))
+                {
+                    string name = descriptor.Name;
+                    object value = descriptor.GetValue(attr);
+                    Console.WriteLine("{0}={1}", name, value);
+                }
+            }
+
+            foreach (var attribute in record.Attributes.Where(t => t.AttributeType == AttributeType.Data))
+            {
+                if (attribute.IsResident == false)
+                {
+                    Console.WriteLine("----- attribute.ContentOffset: {0:X}", attribute.ContentOffset);
+                    foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(attribute))
+                    {
+                        string name = descriptor.Name;
+                        object value = descriptor.GetValue(attribute);
+                        Console.WriteLine("{0}={1}", name, value);
+                    }
+
+                    FileStream file = File.OpenWrite("mft.bin");
+                    var data = ((Data)attribute).NonResidentData;
+                    var buffer = new byte[boot.BytesPerSector * boot.SectorsPerCluster];
+                    foreach (var run in data.DataRuns)
+                    {
+                        stream.Seek(clusterToByteOffset(run.ClusterOffset, boot), SeekOrigin.Begin);
+                        for (ulong i = 0; i < run.ClustersInRun; i++)
+                        {
+                            var readSize = stream.Read(buffer, 0, buffer.Length);
+                            if (readSize != buffer.Length)
+                            {
+                                Log.Fatal("Failed to read data");
+                                Environment.Exit(-1);
+                            }
+                            file.Write(buffer, 0, readSize);
+                        }
+                    };
+                    file.Dispose();
+                }
+                else
+                {
+                    Console.WriteLine("----- resident");
+                    foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(attribute))
+                    {
+                        string name = descriptor.Name;
+                        object value = descriptor.GetValue(attribute);
+                        Console.WriteLine("{0}={1}", name, value);
+                    }
+                }
+            }
+
+            // ProcessRecords(new Dictionary<string, FileRecord>({ "mft", record }, false, false, "C", @".\dump", _mft))
+
+            // _mft = new Mft(stream, false);
+            // Log.Information("FILE records found: {0} (Free records: {1}) File size: {2}", _mft.FileRecords.Count, _mft.FreeFileRecords.Count, _mft.FileSize);
+            // ProcessRecords(_mft.FileRecords, false, false, "C", @".\dump", _mft);
+        }
     }
 
     private static void ProcessRecords(Dictionary<string, FileRecord> records, bool includeShort, bool alltimestamp, string bdl, string drDumpDir, Mft mft)
